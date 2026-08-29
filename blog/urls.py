975337f98a9c4cf1c6d7e3ea267e4c5269a87c51
shruti jindal <shrutijindal326@gmail.com>
2026-08-29 15:39:29 +0100

    configure url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.my_blog, name='blog'),
+    path("admin/", admin.site.urls),
 ]
